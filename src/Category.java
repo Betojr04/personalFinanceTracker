67e4e6a1092e6b1f895aae1ca46b04ef2a986cc3
Betojr04 <betomasia12@gmail.com>
2024-01-14 20:51:39 -0700

    added getter and setter for the category names class for managing the categories instead of just labeling them maintaiing a list of all the transactions tracking each budget for each category and comparing when generating reports this class will help group transactionsand summarize financial data based on the transactions

--- src/Category.java
+++ src/Category.java
@@ -1,13 +1,28 @@
 public class Category {
     private String name;
+    private double budget;
 
     public Category(String name) {
         this.name = name;
+        this.budget = 0.0;
     }
 
-    //GETTERS
+    public void setBudget(double budget){
+        this.budget = budget;
+    }
 
-    //SETTERS
+    public double getBudget(){
+        return this.budget;
+    }
+    public String getName() {
+        return name;
+//        GETTER FOR THE CATEGORY NAME
+    }
+
+    public void setName(String name) {
+        this.name = name;
+//        SETTER FOR THE CATEGORY NAME
+    }
 }
 
 
